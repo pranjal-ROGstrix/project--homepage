63c463551632143f3290dcf727469d622fa8f278
Pranjal Maurya <pranjalmaurya01@gmail.com>
2020-08-19 07:11:37 +0530

    Styled with SASS

--- frontend/backend_url.js
+++ frontend/backend_url.js
@@ -1,2 +1,1 @@
-// window.backend_url = "http://d9ad16e75200.ngrok.io/student/";
-window.backend_url = "http://7e3a92b982b1.ngrok.io/student/";
+window.backend_url = "http://c290c7e76709.ngrok.io/student/";
